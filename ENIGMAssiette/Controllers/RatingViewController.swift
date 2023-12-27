9feb32df765af9189557e3ee4928e0c396a2b741
clement-jny <82049263+clement-jny@users.noreply.github.com>
2023-12-27 23:59:07 +0100

    initial commit

---
+++ ENIGMAssiette/Controllers/RatingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RatingController.swift
+//  ENIGMAssiette
+//
+//  Created by Clément Jaunay on 22/09/2023.
+//
+
+import UIKit
+
+class RatingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
